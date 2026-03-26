9377799d8d251b3b521440ae697ad3775f31daaa
Rabelo69 <arthur.2009rodriguesrabelo@gmail.com>
2026-03-26 13:49:50 -0300

    Update traffic light control and button interaction
    Refactor traffic light control logic and improve button handling.

--- main rabelucho.c
+++ main rabelucho.c
@@ -1,67 +1,60 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <stdio.h>
 
+#define BOTAO 0        
+#define LED_CARROS 2  
+#define LED_PEDESTRE 4
 
-#define TRF_3_botao       0        
-#define TRF_3_ledCarros   2   
-#define TRF_3_ledPedestre 4 
+#define TEMPO_VERDE_CARROS 10
+#define TEMPO_VERDE_PEDESTRE 5
+#define TEMPO_COOLDOWN 8   
 
-
-#define TRF_3_tempoVerdeCarros   13   
-#define TRF_3_tempoVerdePedestre 8    
-#define TRF_3_tempoCooldown      10
-#define TRF_3_reducaoBotao       1
-
-volatile int TRF_3_cooldown = 0;
-
-
-static void IRAM_ATTR TRF_3_isr_handler(void* arg) {
-if (TRF_3_cooldown == 0) {
-TRF_3_cooldown = TRF_3_tempoCooldown; 
-}
-}
+volatile int cooldown = 0;
 
 void app_main(void) {
+gpio_reset_pin(BOTAO);
+gpio_set_direction(BOTAO, GPIO_MODE_INPUT);
+gpio_set_pull_mode(BOTAO, GPIO_PULLDOWN_ONLY);
 
-gpio_reset_pin(TRF_3_botao);
-gpio_set_direction(TRF_3_botao, GPIO_MODE_INPUT);
-gpio_set_pull_mode(TRF_3_botao, GPIO_PULLDOWN_ONLY);
-gpio_set_intr_type(TRF_3_botao, GPIO_INTR_POSEDGE);
-gpio_install_isr_service(0);
-gpio_isr_handler_add(TRF_3_botao, TRF_3_isr_handler, NULL);
+gpio_reset_pin(LED_CARROS);
+gpio_set_direction(LED_CARROS, GPIO_MODE_OUTPUT);
 
+gpio_reset_pin(LED_PEDESTRE);
+gpio_set_direction(LED_PEDESTRE, GPIO_MODE_OUTPUT);
 
-gpio_reset_pin(TRF_3_ledCarros);
-gpio_set_direction(TRF_3_ledCarros, GPIO_MODE_OUTPUT);
-
-gpio_reset_pin(TRF_3_ledPedestre);
-gpio_set_direction(TRF_3_ledPedestre, GPIO_MODE_OUTPUT);
-
-int TRF_3_contador;
+int contador;
 
 while (1) {
+        
+gpio_set_level(LED_CARROS, 0);
+gpio_set_level(LED_PEDESTRE, 1);
+printf("Carros liberados (verde/amarelo), pedestres vermelho.\n");
 
-gpio_set_level(TRF_3_ledCarros, 1);
-gpio_set_level(TRF_3_ledPedestre, 0);
+contador = TEMPO_VERDE_CARROS;
 
-TRF_3_contador = TRF_3_tempoVerdeCarros;
-while (TRF_3_contador > 0) {
+while (contador > 0) {
+            
+if (gpio_get_level(BOTAO) == 1) {
+if (cooldown == 0 && contador > 3) {
+contador = 3; 
+cooldown = TEMPO_COOLDOWN;
+printf("Botão pressionado: reduzindo tempo do semáforo!\n");
+} else {
+printf("Botão em cooldown, aguarde %d segundos...\n", cooldown);
+}
+}
 vTaskDelay(1000 / portTICK_PERIOD_MS);
-TRF_3_contador--;
-
-if (TRF_3_cooldown > 0) {
-TRF_3_cooldown--;
-if (TRF_3_contador > 2) {
-TRF_3_contador -= TRF_3_reducaoBotao; 
+contador--;           
+if (cooldown > 0) {
+cooldown--;
 }
-}
-}
+}        
+gpio_set_level(LED_CARROS, 1);
+gpio_set_level(LED_PEDESTRE, 0);
+printf("Carros vermelho, pedestres verde!\n");
 
-    
-gpio_set_level(TRF_3_ledCarros, 0);
-gpio_set_level(TRF_3_ledPedestre, 1);
-
-vTaskDelay(TRF_3_tempoVerdePedestre * 1000 / portTICK_PERIOD_MS);
+vTaskDelay(TEMPO_VERDE_PEDESTRE * 1000 / portTICK_PERIOD_MS);
 }
 }
